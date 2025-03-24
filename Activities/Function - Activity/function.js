c6522efd4739e468b4b21b477e9151cc05871e72
LanceOnes <johnleonida747@gmail.com>
2025-03-23 20:18:29 -0700

    Activities

---
+++ Activities/Function - Activity/function.js
@@ -0,0 +1,14 @@
+function whatShallIwear(temp){
+    if(temp <60){
+        console.log("Wear a jacket");
+    
+    }else if (temp <70){
+        console.log("Wear a sweater");
+    }else{
+        console.log("Wear a t-shirt");
+    }
+}
+
+whatShallIwear(50);
+whatShallIwear(60);
+whatShallIwear(70);
